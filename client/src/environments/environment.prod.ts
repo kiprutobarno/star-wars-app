373e838910d969d4b55f77fb3c4bf265a9408abe
ywalakamar <barxwells@gmail.com>
2022-06-06 11:29:24 +0300

    chore(hosting): deploy client to Firebase servers
    - [x] [Finishes #182386178]

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCyMX_RE5xVQN89yCXfo4NU4Nm96yyM9Pc',
+    authDomain: 'star-wars-client-852a6.firebaseapp.com',
+    projectId: 'star-wars-client-852a6',
+    storageBucket: 'star-wars-client-852a6.appspot.com',
+    messagingSenderId: '624122541355',
+    appId: '1:624122541355:web:fc29627aa917b1848a016e',
+    measurementId: 'G-EEYLWK7KZT',
+  },
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCyMX_RE5xVQN89yCXfo4NU4Nm96yyM9Pc',
+    authDomain: 'star-wars-client-852a6.firebaseapp.com',
+    projectId: 'star-wars-client-852a6',
+    storageBucket: 'star-wars-client-852a6.appspot.com',
+    messagingSenderId: '624122541355',
+    appId: '1:624122541355:web:fc29627aa917b1848a016e',
+    measurementId: 'G-EEYLWK7KZT',
+  },
 };
 
 /*
