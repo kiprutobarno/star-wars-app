7825051c44803005b58b7d8f7098bb194d85acac
ywalakamar <barxwells@gmail.com>
2022-06-06 13:40:45 +0300

    fixes(configs): add firebase configs
    - [x] WIP

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCyMX_RE5xVQN89yCXfo4NU4Nm96yyM9Pc',
+    authDomain: 'star-wars-client-852a6.firebaseapp.com',
+    projectId: 'star-wars-client-852a6',
+    storageBucket: 'star-wars-client-852a6.appspot.com',
+    messagingSenderId: '624122541355',
+    appId: '1:624122541355:web:fc29627aa917b1848a016e',
+    measurementId: 'G-EEYLWK7KZT',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
